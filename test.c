2cb39b069f7dad49c81ff184bba1ec00299d9574
Ugur <ugurkoc.uk@hotmail.com>
2015-04-11 22:06:36 -0400

    include fix
    include fix

--- test.c
+++ test.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #include <math.h>
 #include "sa_solver.c"
-#include "greedy_solver.c"
+#include "dfs_solver.c"
 
 void printPuzzle(int puzzle[][SIZE]){
    printf("\n+--------+--------+--------+\n");
